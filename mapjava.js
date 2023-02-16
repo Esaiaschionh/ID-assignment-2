2d4ad4db6285265a5478339328fbc184c38126b5
hazelgohh <s10247565@connect.np.edu.sg>
2023-02-16 17:40:16 +0800

    updated java

--- mapjava.js
+++ mapjava.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-    $(".marker-NorthAmercia").click(function(){
-      $(".Na-text").toggle();
-    });
+  $(".hide").hide();
+  
+  
+  $(".marker-NorthAmerica").click(function(){
+    $(".hide").hide();
+    $(".Na-txt").fadeIn(300);
   });
+  
+    $(".marker-Europe").click(function(){
+       $(".hide").hide();
+    $(".Eur-txt").fadeIn(300);
+  });
+  
+    $(".marker-Asia").click(function(){
+       $(".hide").hide();
+    $(".Asia-txt").fadeIn(300);
+  });
+  
+    $(".marker-Africa").click(function(){
+       $(".hide").hide();
+    $(".Afr-txt").fadeIn(300);
+  });
+  
+  $(".marker-SouthAmerica").click(function(){
+       $(".hide").hide();
+    $(".SA-txt").fadeIn(300);
+  });
+  
+  $(".marker-Australia").click(function(){
+       $(".hide").hide();
+    $(".Aus-txt").fadeIn(300);
+  });
+  
+});
